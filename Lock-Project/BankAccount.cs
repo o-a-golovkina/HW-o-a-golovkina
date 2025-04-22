9b94ff7e5ee7e20a9e08acedc32777085fdcff71
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2025-04-22 19:41:35 +0300

    Create a project to demonstrate Concurrency Pattern (Lock)

---
+++ Lock-Project/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿namespace Lock_Project
+{
+    public class BankAccount(decimal initialBalance)
+    {
+        private decimal balance = initialBalance;
+        private readonly object lockObject = new();
+
+        // Метод для зняття грошей з рахунку
+        public void Withdraw(decimal amount)
+        {
+            lock (lockObject) // Забезпечуємо, щоб лише один потік міг виконувати цей код одночасно
+            {
+                if (balance >= amount)
+                {
+                    Console.WriteLine($"Withdrawing {amount}...");
+                    balance -= amount;
+                    Console.WriteLine($"{amount} was withdrawn. Balance: {balance}");
+                }
+                else
+                {
+                    Console.WriteLine($"Insufficient funds for withdrawal {amount}. Balance: {balance}");
+                }
+            }
+        }
+
+        // Метод для перевірки балансу
+        public decimal GetBalance()
+        {
+            lock (lockObject)
+            {
+                return balance;
+            }
+        }
+    }
+}
---
+++ Lock-Project/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lock_Project;
+
+// Створюємо банківський рахунок з початковим балансом
+BankAccount account = new(1000);
+
+// Створюємо кілька потоків, які намагаються зняти гроші
+Thread thread1 = new(() => WithdrawMoney(account, 500));
+Thread thread2 = new(() => WithdrawMoney(account, 700));
+Thread thread3 = new(() => WithdrawMoney(account, 300));
+
+// Запускаємо потоки
+thread1.Start();
+thread2.Start();
+thread3.Start();
+
+// Чекаємо, поки всі потоки завершаться
+thread1.Join();
+thread2.Join();
+thread3.Join();
+
+// Метод для зняття грошей в окремому потоці
+static void WithdrawMoney(BankAccount account, decimal amount)
+{
+    account.Withdraw(amount);
+}
